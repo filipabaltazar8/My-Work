f91ec1711e8165f0f258bd1e2b63774ce5efad74
filipabaltazar8 <59391260+filipabaltazar8@users.noreply.github.com>
2020-01-07 11:38:14 +0100

    ex7.1

---
+++ byMyOwn7/src/byMyOwn7/ex701.java
@@ -0,0 +1,48 @@
+package byMyOwn7;
+
+import com.sun.tools.sjavac.server.SysInfo;
+
+public class ex701 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Say a number between 0 and 10: ");
+		int number;
+		number = keyboard.nextInt();
+				
+		if ( 0>number || number>10);
+		System.out.println("Error");
+
+		else if (number==1);
+			System.out.println("I");
+		
+		else if (number==2);
+		System.out.println("II");
+		
+		else if (number==3);
+		System.out.println("III");
+		
+		else if (number==4);
+		System.out.println("IV");
+		
+		else if (number==5);
+		System.out.println("V");
+		
+		else if (number==6);
+		System.out.println("VI");
+		
+		else if (number==7);
+		System.out.println("VII");
+		
+		else if (number==8);
+		System.out.println("VIII");
+		
+		else if (number==9);
+		System.out.println("IX");
+		
+		else (number==10);
+		System.out.println("X");
+	}
+
+}
---
+++ byMyOwn7/src/module-info.java
@@ -0,0 +1,2 @@
+module byMyOwn7 {
+}
